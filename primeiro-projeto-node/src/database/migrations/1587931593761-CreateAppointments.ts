46dfdd5a5358fcda0d2357370e4e91c06f3a1273
Jefferson Candido <jefferson.ufu@gmail.com>
2020-04-26 20:29:59 -0300

    Migration para definir o padrão de geração de uuid's

--- primeiro-projeto-node/src/database/migrations/1587931593761-CreateAppointments.ts
+++ primeiro-projeto-node/src/database/migrations/1587931593761-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1587931593761 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
