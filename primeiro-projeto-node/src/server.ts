9ead938f6b93bf38ddecf9c6de5fda555b70f130
Jefferson Candido <jefferson.ufu@gmail.com>
2020-04-26 16:45:19 -0300

    Configuração do TypeORM para conexão ao banco de dados

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
@@ -9,4 +11,3 @@ app.use(routes);
 app.listen(3333, () => {
   console.log("Server started on port 3333!");
 });
-
