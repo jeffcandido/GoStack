9f86325597f4e7d8dc24ec8ee1e1659ca9fb37cf
Jefferson Candido <jefferson.ufu@gmail.com>
2020-05-12 01:43:44 -0300

    Event Handling global sem uso de try catch

--- primeiro-projeto-node/src/routes/users.routes.ts
+++ primeiro-projeto-node/src/routes/users.routes.ts
@@ -10,23 +10,20 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const { name, email, password } = request.body;
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const createUser = new CreateUserService();
 
-    delete user.password;
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  delete user.password;
+
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -34,8 +31,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
       const user = await updateUserAvatar.execute({
         user_id: request.user.id,
@@ -45,9 +41,6 @@ usersRouter.patch(
       delete user.password;
 
       return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
-});
+    });
 
 export default usersRouter;
