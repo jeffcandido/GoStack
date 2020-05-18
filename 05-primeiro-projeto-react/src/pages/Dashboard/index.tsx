8bfd43aa797098e1f1cbb477139e85fb683ec0fe
Jefferson Candido <jefferson.ufu@gmail.com>
2020-05-17 22:31:07 -0300

    Criando Rotas, Utilizando Styled Components, Estilizando Dashboard

---
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="testes">
+          <img
+            src="https://avatars0.githubusercontent.com/u/6697374?s=460&v=4"
+            alt="Jefferson Cândido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="testes">
+          <img
+            src="https://avatars0.githubusercontent.com/u/6697374?s=460&v=4"
+            alt="Jefferson Cândido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="testes">
+          <img
+            src="https://avatars0.githubusercontent.com/u/6697374?s=460&v=4"
+            alt="Jefferson Cândido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
