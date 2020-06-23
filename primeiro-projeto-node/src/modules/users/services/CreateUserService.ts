70c0e89c1e5912950765e1b809d42f9004f45238
Jefferson Candido <jefferson.ufu@gmail.com>
2020-06-23 16:15:46 -0300

    Arquitetura e DDD

---
+++ primeiro-projeto-node/src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import User from "../infra/typeorm/entities/User";
+import { hash } from "bcryptjs";
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from "../repositories/IUsersRepository";
+import { injectable, inject } from 'tsyringe';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+    ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email address already used.')
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
--- primeiro-projeto-node/src/shared/infra/http/server.ts
+++ primeiro-projeto-node/src/shared/infra/http/server.ts
@@ -1,14 +1,12 @@
 import 'reflect-metadata';
-
 import express, { NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-
 import routes from './routes';
-import uploadConfig from './config/upload';
-
-import './database';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
+import '@shared/container';
 
 const app = express();
 
