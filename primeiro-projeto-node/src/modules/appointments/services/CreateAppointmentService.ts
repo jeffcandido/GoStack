f42d39a0135161055bc05f8b96bea4e540913444
Jefferson Candido <jefferson.ufu@gmail.com>
2020-07-05 03:10:22 -0300

    Sending Notifications

--- primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,8 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
-import { injectable, inject } from 'tsyringe';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
+import { injectable, inject, delay } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 
 import AppError from '@shared/errors/AppError';
@@ -17,6 +18,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
     ) {}
 
   public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
@@ -49,6 +53,12 @@ class CreateAppointmentService {
       date: appointmentDate
     });
 
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para o dia ${dateFormatted}`,
+    });
 
     return appointment;
   }
