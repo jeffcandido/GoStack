87c5bae64c774b5e509063dc5ecb99bccc6d9458
Jefferson Candido <jefferson.ufu@gmail.com>
2020-07-05 12:34:13 -0300

    Data validation using Celebrate

--- primeiro-projeto-node/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ primeiro-projeto-node/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from 'modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -13,7 +14,23 @@ const providerDayController = new ProviderDayController();
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+  providerMonthController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayController.index,
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
+);
 
 export default providersRouter;
