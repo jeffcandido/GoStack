144ec6c19f4f588495022879242e779b2f4253ac
Jefferson Candido <jefferson.ufu@gmail.com>
2020-07-05 12:58:40 -0300

    Handling environment variables with dotenv.

--- primeiro-projeto-node/src/config/auth.ts
+++ primeiro-projeto-node/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '790572a73246e13a4c1c7b7e71d3d6cb',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- primeiro-projeto-node/src/shared/infra/http/server.ts
+++ primeiro-projeto-node/src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 
 import express, { NextFunction } from 'express';
 import cors from 'cors';
